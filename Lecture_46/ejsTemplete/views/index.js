d5cb3dd542a3f8c6742f8268b41138e10e070168
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2026-04-14 11:45:41 +0530

    add: ejs start

---
+++ Lecture_46/ejsTemplete/views/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(8080, () => {
+    console.log("Backend is running at 8080");
+})
+
+// to view i need to setup the templete
+app.set("view engine", "ejs");
+
+// bu default viwe path is process.cwd + ./views
+app.set("views", path.join(__dirname, 'views'));
+
+// render the dynamic content in the browser
+// root route
+app.get('/', (req, res) => {
+    res.render("index");
+})
+
+
+appget('/random',(res,req)=>{
+    res.render("random");
+    
+})
+
+
+
+
+
+
+
+
+
+
